1cfc99a1f937cb27038df10993b9ef97ab947c88
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-04-06 20:59:47 +0200

    new public build

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7420d0af34ae7b3d8727c03371376021.js"
+  "/precache-manifest.7ab8c15e25490323d05d363f951b7088.js"
 );
 
 workbox.clientsClaim();
