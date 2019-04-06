16cff1e85a05f9263bc1b1d7c8e40b7a024b0b5f
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-04-06 15:16:55 +0200

    prepared for heroku

--- frontend/src/Apollo.js
+++ frontend/src/Apollo.js
@@ -1,31 +1,6 @@
-// import { ApolloClient } from 'apollo-client';
-// import { InMemoryCache } from 'apollo-cache-inmemory';
-// import { HttpLink } from 'apollo-link-http';
-// import { onError } from 'apollo-link-error';
-// import { ApolloLink } from 'apollo-link';
-
 import ApolloClient from "apollo-boost";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
-  //uri: "/graphql"
+  //uri: "http://localhost:4000/graphql"
+  uri: "/graphql"
 });
-
-// export const client = new ApolloClient({
-//   link: ApolloLink.from([
-//     onError(({ graphQLErrors, networkError }) => {
-//       if (graphQLErrors)
-//         graphQLErrors.map(({ message, locations, path }) =>
-//           console.log(
-//             `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-//           ),
-//         );
-//       if (networkError) console.log(`[Network error]: ${networkError}`);
-//     }),
-//     new HttpLink({
-//       uri: 'http://localhost:4000/graphql',
-//       credentials: 'same-origin',
-//      })
-//   ]),
-//   cache: new InMemoryCache()
-// });
