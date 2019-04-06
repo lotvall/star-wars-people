fb0a5fc149bab25626721bae65e4bbd073d15ef3
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-04-06 16:39:33 +0200

    added schema, resolvers, helpers to build

---
+++ build/resolvers/planets.js
@@ -0,0 +1,87 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _axios = require('axios');
+
+var _axios2 = _interopRequireDefault(_axios);
+
+var _dataloader = require('dataloader');
+
+var _dataloader2 = _interopRequireDefault(_dataloader);
+
+var _getCategoryFromUrl = require('../helpers/getCategoryFromUrl');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+var UrlLoader = new _dataloader2.default(function (urls) {
+  return Promise.all(urls.map(getFromUrl));
+});
+var getFromUrl = function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(url) {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return (0, _axios2.default)(url);
+
+          case 2:
+            return _context.abrupt('return', _context.sent);
+
+          case 3:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, undefined);
+  }));
+
+  return function getFromUrl(_x) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+exports.default = {
+
+  Query: {
+    planets: function () {
+      var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(parent, _ref3, context, info) {
+        var url = _ref3.url;
+
+        var _ref4, data;
+
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.next = 2;
+                return UrlLoader.load(url);
+
+              case 2:
+                _ref4 = _context2.sent;
+                data = _ref4.data;
+
+                console.log(data);
+                return _context2.abrupt('return', data);
+
+              case 6:
+              case 'end':
+                return _context2.stop();
+            }
+          }
+        }, _callee2, undefined);
+      }));
+
+      return function planets(_x2, _x3, _x4, _x5) {
+        return _ref2.apply(this, arguments);
+      };
+    }()
+  }
+
+};
+//# sourceMappingURL=planets.js.map
