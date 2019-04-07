48e37904996422675afd5a040bd1a6de5ba7ed69
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-04-07 17:24:21 +0200

    get /* instead of /

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(graphQLpath, express.static('public'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build/public'));
-  app.get('/', (request, response) => {
+  app.get('/*', (request, response) => {
     response.sendFile(path.join(__dirname, 'build/public', 'index.html'));
   });
 }
